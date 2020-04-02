6da32034c21d1ae13cf7fa951435a0e430920af3
raj1020 <cas021911@coderacademy.edu.au>
2020-04-03 10:44:27 +1100

    Added ratings to the crafts

--- ContosoCrafts.Website/Controllers/ProductsController.cs
+++ ContosoCrafts.Website/Controllers/ProductsController.cs
@@ -26,6 +26,17 @@ namespace ContosoCrafts.Website.Controllers
         {
             return ProductService.GetProducts();
         }
+
+        //[HttpPatch]"[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string ProductId, [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+
+
     }
 }
 
--- ContosoCrafts.Website/Services/JsonFileProductService.cs
+++ ContosoCrafts.Website/Services/JsonFileProductService.cs
@@ -37,15 +37,16 @@ namespace ContosoCrafts.WebSite.Services
         {
             var products = GetProducts();
 
-            if (products.First(x => x.Id == productId).Ratings == null)
+            var query = products.First(x => x.Id == productId);
+            if (query.Ratings == null)
             {
-                products.First(x => x.Id == productId).Ratings = new int[] { rating };
+                query.Ratings = new int[] { rating };
             }
             else
             {
-                var ratings = products.First(x => x.Id == productId).Ratings.ToList();
+                var ratings = query.Ratings.ToList();
                 ratings.Add(rating);
-                products.First(x => x.Id == productId).Ratings = ratings.ToArray();
+                query.Ratings = ratings.ToArray();
             }
 
             using (var outputStream = File.OpenWrite(JsonFileName))
@@ -55,10 +56,35 @@ namespace ContosoCrafts.WebSite.Services
                     {
                         SkipValidation = true,
                         Indented = true
+
                     }),
                     products
-                );
+                    );
+                
             }
+
+            //if (products.First(x => x.Id == productId).Ratings == null)
+            //{
+            //    products.First(x => x.Id == productId).Ratings = new int[] { rating };
+            //}
+            //else
+            //{
+            //    var ratings = products.First(x => x.Id == productId).Ratings.ToList();
+            //    ratings.Add(rating);
+            //    products.First(x => x.Id == productId).Ratings = ratings.ToArray();
+            //}
+
+            //using (var outputStream = File.OpenWrite(JsonFileName))
+            //{
+            //    JsonSerializer.Serialize<IEnumerable<Product>>(
+            //        new Utf8JsonWriter(outputStream, new JsonWriterOptions
+            //        {
+            //            SkipValidation = true,
+            //            Indented = true
+            //        }),
+            //        products
+            //    );
+            //}
         }
     }
 
